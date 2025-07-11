552e68169f8fa39e1a707803b7f79a676112e541
Karma Riuk <riukkarma@gmail.com>
2025-07-11 11:21:45 +0200

    fixed compiler warning

--- src/ast/expressions/infix.hpp
+++ src/ast/expressions/infix.hpp
@@ -7,8 +7,8 @@ namespace ast {
     struct infix_expr : expression {
         infix_expr(token::token, std::string, ast::expression*);
         token::token token;
-        expression* left;
         std::string op;
+        expression* left;
         expression* right;
 
         std::string token_literal() const override;
