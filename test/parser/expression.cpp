20b2c4a8183a50b909e517a2ab4d2e84a1521537
Karma Riuk <riukkarma@gmail.com>
2025-07-15 20:09:08 +0200

    added checks for operator precedence with function call

--- test/parser/expression.cpp
+++ test/parser/expression.cpp
@@ -241,6 +241,11 @@ TEST_SUITE("Parser: expression") {
             {"2 / (5 + 5)", "(2 / (5 + 5))"},
             {"-(5 + a)", "(-(5 + a))"},
             {"!(true == true)", "(!(true == true))"},
+            {"a + add(b * c) + d", "((a + add((b * c))) + d)"},
+            {"add(a, b, 1, 2 * 3, 4 + 5, add(6, 7 * 8))",
+             "add(a, b, 1, (2 * 3), (4 + 5), add(6, (7 * 8)))"},
+            {"add(a + b + c * d / f + g)",
+             "add((((a + b) + ((c * d) / f)) + g))"},
         };
 
         for (auto& t : tests) {
